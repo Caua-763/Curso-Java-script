c89b38c3d5c6569510cf253ab4c9240c7f0b1ddd
Caua <crskzinvir@gmail.com>
2025-03-31 22:07:46 -0300

    commit

--- Client/App/index.js
+++ Client/App/index.js
@@ -1,32 +1,33 @@
+
 var campos = [
     document.querySelector('#data'),
     document.querySelector('#valor'),
     document.querySelector('#quantidade')
 ];
 
-var tbody = document.querySelector('tbody')
-
 console.log(campos);
 
-var tbody = document.querySelector('table tbody')
-document.querySelector('.form').addEventListener('submit',
-    function(event) {
-        var	tr	=	document.createElement('tr');
-    
-campos.forEach(function(campo) {
+var tbody = document.querySelector('table tbody');
 
-    var td = document.createElement('td');
-    td.textContent = campo.Value;
-    tr.appndChild(td);
-});
+document.querySelector('.form').addEventListener('submit', function (event) {
 
-var tdVolume = document.createElement('td');
-tdVolume.textContent = campos [1].Value * campos[2].value;
-tr.appendChild(tdVolume);		
-tr.appndChild(tr);
-	
-	campos[0].value	=	'';	
-	campos[1].value	=	1;	
-	campos[2].value	=	0;
-	campos[0].focus();
+    event.preventDefault();
+    var tr = document.createElement('tr');
+
+    campos.forEach(function (campo) {
+        var td = document.createElement('td');
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+
+    var tdVolume = document.createElement('td');
+    tdVolume.textContent = campos[1].value * campos[2].value;
+    tr.appendChild(tdVolume);
+
+    tbody.appendChild(tr);
+
+    campos[0].value = '';
+    campos[1].value = 1;
+    campos[2].value = 0;
+    campos[0].focus();
 });
